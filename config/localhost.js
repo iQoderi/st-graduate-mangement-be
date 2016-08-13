a32cfb52e7ba0f4b82d44a8332eb44f0ee8620e3
qoder <qoder@hrsoft.net>
2016-08-14 03:48:04 +0800

    修复了一些bug

--- config/localhost.js
+++ config/localhost.js
@@ -2,6 +2,6 @@
  * Created by qoder on 16/6/8.
  */
 // const  host='http://192.168.16.55:3939';
-const  host='http://192.168.1.100:3939';
-
+// const  host='http://192.168.1.100:3939';
+const  host='http://api.neuqst.qoder.cn';
 module.exports=host;
