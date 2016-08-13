8d8a84584e0774604bc9b357225d3bf5200dfec8
qoder <qoder@hrsoft.net>
2016-08-14 07:22:23 +0800

    修复了导入excel的bug（pm2坑!!!npm start没问题，pm2 start 就出问题了，坑了我一个通宵!!!!）

--- routes/api/home/excel/importExcel.js
+++ routes/api/home/excel/importExcel.js
@@ -8,16 +8,23 @@ const uuid = require('uuid');
 const graduate = require('../../../../models/Graduates');
 const graduateModel = mongoose.model('graduate');
 const xlsx=require('node-xlsx');
+const path=require('path');
 const parseExcel=require('../../../../library/parseExcel');
 
-
 function uploadExcel(req,res,next) {
+    // console.log(__dirname,'called');
+
     var form = new formidable.IncomingForm();   //创建上传表单
     form.encoding = 'utf-8';        //设置编辑
-    form.uploadDir = 'public/excel/';     //设置上传目录
+    // form.uploadDir = 'public/excel/';     //设置上传目录
+    form.uploadDir =__dirname+ '/../../../../public/excel/';     //设置上传目录
     form.keepExtensions = true;     //保留后缀
-    form.maxFieldsSize = 5 * 1024 * 1024;   //文件大小
-    form.parse(req, function (err, fields, files) {
+    form.maxFieldsSize = 50 * 1024 * 1024;   //文件大小
+
+    form.on('error',(err)=>{
+        console.log(err);
+    })
+    form.parse(req,  (err, fields, files)=> {
         if (err) {
             res.json({
                 code:90017,
@@ -38,60 +45,71 @@ function uploadExcel(req,res,next) {
         var nameArray = filename.split('.');
         var type = nameArray[nameArray.length - 1];
         var name="";
-        nameArray.forEach((each)=>{
-            name+=each;
+        nameArray.forEach((each,index)=>{
+            if(index<nameArray.length-1){
+                name+=each;
+            }
         });
 
-        var excelName = name + uuid.v4() + '.' + type;
+        var excelName=`${name}_${uuid.v4()}.${type}`;
         var newPath = form.uploadDir + excelName;
-        fs.renameSync(files.excel.path, newPath);  //重命名
-        const workSheetsFromFile = xlsx.parse(fs.readFileSync(newPath));
-        console.log(workSheetsFromFile);
-        if(workSheetsFromFile.length<1){
-            res.json({
-                code:90017,
-                data:{
-                    msg:'上传失败'
-                }
-            })
-        }else{
-            const data=parseExcel(res,workSheetsFromFile[0].data);
-            if(Array.isArray(data)){
-                var SUCC_COOUNT=0;
-                var importPromise=new Promise((resolve,reject)=>{
-                    data.forEach((each)=>{
-                        var graduate = new graduateModel(each);
-                        graduate.save((err)=>{
-                            if(err){
-                                reject(SUCC_COOUNT);
-                            }else{
-                                SUCC_COOUNT++;
-                            }
-                        })
-                    });
-
-                    resolve(data.length);
-                });
-
-                importPromise.then((count)=>{
+        fs.rename(files.excel.path, newPath,function (err) {
+            if(err){
+                res.json({
+                    code:90017,
+                    data:{
+                        msg:'导入失败'
+                    }
+                })
+            }else{
+                const workSheetsFromFile = xlsx.parse(fs.readFileSync(newPath));
+                if(workSheetsFromFile.length<1){
                     res.json({
-                        code:10000,
+                        code:90017,
                         data:{
-                            msg:'上传成功',
-                            count:count
+                            msg:'导入失败'
                         }
                     })
-                }).catch((count)=>{
-                    res.json({
-                        code:90018,
-                        data:{
-                            msg:'部分成功',
-                            count:count
-                        }
-                    });
-                });
+                }else{
+                    const data=parseExcel(res,workSheetsFromFile[0].data);
+                    if(Array.isArray(data)){
+                        var SUCC_COOUNT=0;
+                        var importPromise=new Promise((resolve,reject)=>{
+                            data.forEach((each)=>{
+                                var graduate = new graduateModel(each);
+                                graduate.save((err)=>{
+                                    if(err){
+                                        reject(SUCC_COOUNT);
+                                    }else{
+                                        SUCC_COOUNT++;
+                                    }
+                                })
+                            });
+
+                            resolve(data.length);
+                        });
+
+                        importPromise.then((count)=>{
+                            res.json({
+                                code:10000,
+                                data:{
+                                    msg:'导入成功',
+                                    count:count
+                                }
+                            })
+                        }).catch((count)=>{
+                            res.json({
+                                code:90018,
+                                data:{
+                                    msg:'部分成功',
+                                    count:count
+                                }
+                            });
+                        });
+                    }
+                }
             }
-        }
+        });
     });
 }
 
