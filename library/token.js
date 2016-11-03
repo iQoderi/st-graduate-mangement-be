89aaed98e103744f485d94b35951041bb56a75f3
qoder <qoder@hrsoft.net>
2016-11-03 22:22:17 +0800

    改写了token生成算法

--- library/token.js
+++ library/token.js
@@ -1,9 +1,21 @@
 /**
  * Created by qoder on 16/11/3.
  */
-const MD5=require('md5');
-const uuid=require('uuid');
+const MD5 = require('md5');
+const uuid = require('uuid');
+const mongoose = require('mongoose');
+const userModel = require('../models/Users');
+const Users = mongoose.model('User');
 
-function createToken() {
-
+function jwt(createAt, expireIn, userId) {
+    var token = {
+        createAt: createAt,
+        expireIn: expireIn,
+        token: MD5(uuid.v4() + userId)
+    };
+    const condition = {id: userId};
+    const update = {$set: {token: token}};
+    Users.update(condition, update)
 }
+
+module.exports = jwt;
